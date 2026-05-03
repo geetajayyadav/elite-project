24c4c35248ecbe09451103b4ee45ea1ab82c5d93
Ajay Yadav <lmsajay05@gmail.com>
2026-05-03 12:31:56 +0530

    Lambda Test Integrated

--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -2,11 +2,11 @@ package stepdefinitions;
 
 import io.cucumber.java.en.*;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.net.URL;
 import java.time.Duration;
-import java.util.List;
 
 public class LoginSteps {
 
@@ -15,45 +15,41 @@ public class LoginSteps {
     @Given("user is on login page")
     public void user_is_on_login_page() {
 
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://wccqa.on24.com/webcast/login");
+        try {
 
-        System.out.println("Opened URL: " + driver.getCurrentUrl());
+            // 🔥 Replace with your credentials
+            String username = "lmsajay05";
+            String accessKey = "LT_UUaYaPJJBZXinr0KvwL7eDBdFHzJDsCwwTeqnKNGMAhgbeN";
+
+            String gridURL = "https://" + username + ":" + accessKey + "@hub.lambdatest.com/wd/hub";
+
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("browserName", "Chrome");
+            caps.setCapability("browserVersion", "latest");
+
+            caps.setCapability("LT:Options", new java.util.HashMap<String, Object>() {{
+                put("platformName", "Windows 11");
+                put("build", "Elite Project");
+                put("name", "Login Test");
+            }});
+
+            driver = new RemoteWebDriver(new URL(gridURL), caps);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            driver.get("https://wccqa.on24.com/webcast/login");
+
+            System.out.println("Opened on LambdaTest");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @When("user enters username {string} and password {string}")
     public void user_enters_credentials(String username, String password) {
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-
-        // 🔥 FIND ANY TEXT INPUT (REALISTIC APPROACH)
-        List<WebElement> inputs = wait.until(
-                ExpectedConditions.presenceOfAllElementsLocatedBy(By.tagName("input"))
-        );
-
-        WebElement usernameField = null;
-        WebElement passwordField = null;
-
-        for (WebElement input : inputs) {
-            String type = input.getAttribute("type");
-
-            if (type != null && type.equalsIgnoreCase("text")) {
-                usernameField = input;
-            }
-
-            if (type != null && type.equalsIgnoreCase("password")) {
-                passwordField = input;
-            }
-        }
-
-        if (usernameField == null) {
-            throw new RuntimeException("Username field NOT found");
-        }
-
-        if (passwordField == null) {
-            throw new RuntimeException("Password field NOT found");
-        }
+        WebElement usernameField = driver.findElement(By.xpath("//input[@type='text']"));
+        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
 
         usernameField.clear();
         usernameField.sendKeys(username);
@@ -61,22 +57,14 @@ public class LoginSteps {
         passwordField.clear();
         passwordField.sendKeys(password);
 
-        // 🔥 Click button (generic)
-        WebElement loginBtn = wait.until(
-                ExpectedConditions.elementToBeClickable(
-                        By.xpath("//button | //input[@type='submit']")
-                )
-        );
-
+        WebElement loginBtn = driver.findElement(By.xpath("//button | //input[@type='submit']"));
         loginBtn.click();
-
-        System.out.println("Entered: " + username + " / " + password);
     }
 
     @Then("user should see {string}")
     public void user_should_see_result(String result) {
 
-        System.out.println("Expected result: " + result);
+        System.out.println("Result: " + result);
 
         driver.quit();
     }
